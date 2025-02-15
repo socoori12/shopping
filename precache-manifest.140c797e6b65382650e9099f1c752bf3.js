d401bf339d055b3e13dc75590489db2ad376fc20
socoori12 <socoori1@daum.net>
2025-02-15 21:07:27 +0900

    Updates

---
+++ precache-manifest.140c797e6b65382650e9099f1c752bf3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e268db48be3325b86fa5bc6b0d7dc6b",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "bfa215c2ef23bc7b4e59",
+    "url": "/shopping/static/css/main.fc403d5a.chunk.css"
+  },
+  {
+    "revision": "3bbb0ed19d4c37454caa",
+    "url": "/shopping/static/js/2.523b47f8.chunk.js"
+  },
+  {
+    "revision": "6248bae571f9c8e2f15bd5380283436f",
+    "url": "/shopping/static/js/2.523b47f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa215c2ef23bc7b4e59",
+    "url": "/shopping/static/js/main.673d0caa.chunk.js"
+  },
+  {
+    "revision": "dfc87da7a6a6c93b19f2",
+    "url": "/shopping/static/js/runtime-main.86d307e8.js"
+  },
+  {
+    "revision": "3914abbeb0084285d11bdc09313884a3",
+    "url": "/shopping/static/media/bot01.3914abbe.jpg"
+  },
+  {
+    "revision": "c17b0402a5b275635015ff45fc3aa512",
+    "url": "/shopping/static/media/bot02.c17b0402.jpg"
+  },
+  {
+    "revision": "196f94e279735e242a9874ff29fb89f7",
+    "url": "/shopping/static/media/bot03.196f94e2.jpg"
+  },
+  {
+    "revision": "de523bda3684e4b90e2c1a14e4213262",
+    "url": "/shopping/static/media/bot04.de523bda.jpg"
+  }
+]);
